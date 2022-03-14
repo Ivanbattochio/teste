50e4df8621c9745cc8d033a7960ebec26ddfae1e
Ivan <ivanborgo@outlook.com.com>
2022-03-13 23:43:57 -0300

    secret kkk3

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var jsonParser = bodyParser.json();
 
 app.get("/", (req, res) => {
   console.log("hello world");
-  res.send("Hello secret!!");
+  res.send("Hello secret!!!!");
 });
 
 app.post("/teste", jsonParser, (req, res) => {
