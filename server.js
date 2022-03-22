07cffd0682a7c0e5d71b9ac65511edf193a513d6
Ivan <ivanborgo@outlook.com.com>
2022-03-22 11:32:24 -0300

    sudo

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post("/webhooks/update-repo", (req, res) => {
   } else {
     console.log("assinatura valida");
     exec(
-      "cd /plantview/ivan-pipefy-integration/teste && git pull && sleep 5 && npm install && sleep 10 && systemctl restart pipefy-integration",
+      "cd /plantview/ivan-pipefy-integration/teste && git pull && sleep 5 && npm install && sleep 10 && sudo systemctl restart pipefy-integration",
       (error, stdout, stderr) => {
         if (error) {
           console.log(`error: ${error.message}`);
